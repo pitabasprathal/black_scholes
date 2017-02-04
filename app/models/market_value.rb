d9e075c5960ed3846e63ef88493baae15cf1ecaf
pitabas <pitabas.prathal@ajatus.co.in>
2017-02-04 23:27:42 +0530

    user ref added

--- app/models/market_value.rb
+++ app/models/market_value.rb
@@ -1,5 +1,6 @@
 class MarketValue < ApplicationRecord
   belongs_to :direction
+  belongs_to :user
   validates :stock_price, presence: true,  :numericality => {:greater_than =>0}
   validates :strike_price, presence: true,  :numericality => {:greater_than =>0}
   validates :volatility, presence: true,  :numericality => {:greater_than =>0,:less_than=>100}
